01070242e725cbbf21966be637bc71e73db52b28
Olai <gamingtutsdk@gmail.com>
2014-06-11 20:12:43 +0200

    Fixed Heal Command

--- mythology/commands/HealCommand.java
+++ mythology/commands/HealCommand.java
@@ -5,65 +5,80 @@ import java.util.List;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.ChatComponentTranslation;
-import net.minecraft.world.WorldSettings;
 
-public class HealCommand extends CommandBase {
-    public String getCommandName() {
+public class HealCommand extends CommandBase
+{
+    public String getCommandName()
+    {
         return "heal";
     }
 
     /**
      * Return the required permission level for this command.
      */
-    public int getRequiredPermissionLevel() {
+    public int getRequiredPermissionLevel()
+    {
         return 2;
     }
 
-    public String getCommandUsage(ICommandSender par1ICommandSender) {
+    public String getCommandUsage(ICommandSender par1ICommandSender)
+    {
         return "commands.heal.usage";
     }
 
-    public void processCommand(ICommandSender par1ICommandSender, String[] par2ArrayOfStr) {
-        if (par2ArrayOfStr.length < 2) {
-            EntityPlayerMP healedplayer = par2ArrayOfStr.length > 2 ? getPlayer(par1ICommandSender, par2ArrayOfStr[1]) : getCommandSenderAsPlayer(par1ICommandSender);
-            healedplayer.heal(healedplayer.getMaxHealth()-healedplayer.getHealth());;
-            healedplayer.getFoodStats().setFoodLevel(20);
-            healedplayer.fallDistance = 0.0F;
-            healedplayer.extinguish();
-            EntityPlayerMP healer = getCommandSenderAsPlayer(par1ICommandSender);
-            if(healer == healedplayer){
-            	healedplayer.addChatComponentMessage(new ChatComponentText("You have been healed by yourself."));
-            } else {
-            	healedplayer.addChatComponentMessage(new ChatComponentText("You have been healed by " + healer.getDisplayName() + "."));
-            }
-        } else {
-            throw new WrongUsageException("commands.heal.usage", new Object[0]);
+    public void processCommand(ICommandSender par1ICommandSender, String[] stringArray)
+    {
+        EntityPlayerMP sender = getCommandSenderAsPlayer(par1ICommandSender);
+        if (stringArray.length == 0)
+        {
+        	healPlayer(sender, sender);
         }
+        else if (stringArray.length <= 1)
+        {
+        	if (stringArray[0].toLowerCase().equals("me")){
+            	healPlayer(sender, sender);
+            } else {
+            	EntityPlayerMP target = getPlayer(par1ICommandSender, stringArray[0]);
+            	healPlayer(sender, target);
+            }
+        }
+    }
+    
+    private void healPlayer(EntityPlayerMP healer, EntityPlayerMP healed){
+    	if (healer == healed){
+    		healer.addChatComponentMessage(new ChatComponentText("You healed yourself."));
+    	} else {
+    		healer.addChatComponentMessage(new ChatComponentText("You healed " + healed.getCommandSenderName() + "."));
+    		healed.addChatComponentMessage(new ChatComponentText("You have been healed by " + healer.getCommandSenderName() + "."));
+    	}
+    	healed.heal(Float.MAX_VALUE);
+    	healed.extinguish();
+    	healed.getFoodStats().setFoodLevel(20);
+    	healed.fallDistance = 0.0F;
+    	healed.setAir(20);
     }
 
     /**
      * Adds the strings available in this command to the given list of tab completion options.
      */
-    public List addTabCompletionOptions(ICommandSender par1ICommandSender, String[] par2ArrayOfStr) {
-        return par2ArrayOfStr.length == 1 ? getListOfStringsMatchingLastWord(par2ArrayOfStr, this.getListOfPlayerUsernames()) : null;
+    public List addTabCompletionOptions(ICommandSender par1ICommandSender, String[] stringArray)
+    {
+        return stringArray.length == 1 ? getListOfStringsMatchingLastWord(stringArray, this.getAllUsernames()) : null;
     }
 
-    /**
-     * Returns String array containing all player usernames in the server.
-     */
-    protected String[] getListOfPlayerUsernames() {
+    protected String[] getAllUsernames()
+    {
         return MinecraftServer.getServer().getAllUsernames();
     }
 
     /**
      * Return whether the specified command parameter index is a username parameter.
      */
-    public boolean isUsernameIndex(String[] par1ArrayOfStr, int par2) {
+    public boolean isUsernameIndex(String[] par1ArrayOfStr, int par2)
+    {
         return par2 == 1;
     }
 }
